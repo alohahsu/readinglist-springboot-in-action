12d7fa5a02ccf4d8f9a09ef489e6d44085a077bc
AlohaHsu <bostonhsu@icloud.com>
2021-10-13 23:40:05 +0800

    Correct Book.java.

--- src/main/java/com/manning/readinglist/Book.java
+++ src/main/java/com/manning/readinglist/Book.java
@@ -7,8 +7,9 @@ import javax.persistence.Id;
 
 @Entity
 public class Book {
+
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long id;
 	private String reader;
 	private String isbn;
@@ -63,4 +64,5 @@ public class Book {
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 }

32a99061b2ba00ce9f583d2ee38bea55d90cd79c
AlohaHsu <bostonhsu@icloud.com>
2021-10-15 15:01:43 +0800

    目的：给安全配置类里打开一个接口，允许spring.profiles.active的属性值注入。

--- src/main/java/com/manning/readinglist/SecurityConfig.java
+++ src/main/java/com/manning/readinglist/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.manning.readinglist;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.domain.Example;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,6 +12,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+@Profile("production")
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
